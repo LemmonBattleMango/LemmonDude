bcd008cfa2df32f2e76fa4ceac48054807b5684b
LemmonPlustech <iristudi@gmail.com>
2015-04-18 18:03:08 -0400

    cambio escala etapa y controles

--- Client/Assets/Scripts/JoystickController.cs
+++ Client/Assets/Scripts/JoystickController.cs
@@ -4,24 +4,24 @@ using System.Collections;
 public class JoystickController : MonoBehaviour {
 
 	private KeyCode jumpKey = KeyCode.Space;
-	private KeyCode fireKey = KeyCode.Return;
+	private KeyCode fireKey = KeyCode.LeftAlt;
 
 	//======================================================
 	public Vector2 GetDirection() {
 		Vector2 direction = Vector2.zero;
-		if( Input.GetKey( KeyCode.A ) ) {
+		if( Input.GetKey( KeyCode.LeftArrow ) ) {
 			direction.x = -1;
 		}
 		
-		if( Input.GetKey( KeyCode.D ) ) {
+		if( Input.GetKey( KeyCode.RightArrow ) ) {
 			direction.x = 1;
 		}
 		
-		if( Input.GetKey( KeyCode.W ) ) {
+		if( Input.GetKey( KeyCode.UpArrow ) ) {
 			direction.y = 1;
 		}
 		
-		if( Input.GetKey( KeyCode.S ) ) {
+		if( Input.GetKey( KeyCode.DownArrow ) ) {
 			direction.y = -1;
 		}
 		direction.Normalize();
