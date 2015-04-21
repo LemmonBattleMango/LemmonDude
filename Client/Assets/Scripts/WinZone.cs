bbe2f403539a54f10a5f103881d43def77c82000
Felipe Rodillo <frodillo@dena.jp>
2015-04-20 22:30:23 -0300

    win zone now works

--- Client/Assets/Scripts/WinZone.cs
+++ Client/Assets/Scripts/WinZone.cs
@@ -6,10 +6,10 @@ public class WinZone : MonoBehaviour {
 	private const float DURATION_SECS = 3f;
 
 	//======================================================
-	private void OnTriggerStay2D( Collider2D other ) {	
+	private void OnTriggerEnter2D( Collider2D other ) {	
 		
 		PlayerController player = other.GetComponent<PlayerController>();
-		if( player == null || !player.isDead ) {
+		if( player == null || player.isDead ) {
 			return;
 		}
 		
@@ -18,9 +18,9 @@ public class WinZone : MonoBehaviour {
 
 	//======================================================
 	private IEnumerator WinCoroutine( PlayerController player ) {
-		MinigameTimeManager.instance.timeScale = 0.1f;
+		MinigameTimeManager.instance.timeScale = 0.3f;
 		player.joystickController.ForceDirection( Vector2.zero, DURATION_SECS );
 		yield return new WaitForSeconds( DURATION_SECS );
-		Application.LoadLevel( "SplashScene" );
+		Application.LoadLevel( "SplashScreen" );
 	}
 }
