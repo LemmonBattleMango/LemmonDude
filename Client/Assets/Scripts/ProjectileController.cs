f565c5625af152fe52af228431d277ed199ba9e4
Felipe Rodillo <frodillo@dena.jp>
2015-05-18 22:49:08 -0400

    ninja bullet travels a fixed distance

--- Client/Assets/Scripts/ProjectileController.cs
+++ Client/Assets/Scripts/ProjectileController.cs
@@ -5,30 +5,33 @@ public class ProjectileController : MonoBehaviour {
 	private const float OFF_SCREEN_MARGIN = 0.5f;
 	private Vector2 direction;
 	public float projectilePushBackForce = 0.15f;
-	public float lifeSpan = 3f;
+	//public float lifeSpan = 3f;
+	public float maxTraveledDistance = 3f;
+
 
 	public virtual float speed{ get{ return GlobalConfig.instance.projectileVelocity; } }
 
 	private PlayerController owner;
+	private float traveledDistance = 0f;
 
 	// ====================================================
 	public void Configure( Vector2 _direction, PlayerController _owner ) {
 		gameObject.layer = LayerMask.NameToLayer( "BulletLayer" );
 		direction = _direction.normalized;
 		owner = _owner;
-		StartCoroutine( AutoDestroyCoroutine() );
+		//StartCoroutine( AutoDestroyCoroutine() );
 	}
 
-	//======================================================
-	public IEnumerator AutoDestroyCoroutine() {
-		//wait for one frame to be on screen
-		yield return null;
-		float initialTime = MinigameTimeManager.instance.time;
-		while( MinigameTimeManager.instance.time < initialTime + lifeSpan && IsOnScreen() ) {
-			yield return null;
-		}
-		Destroy( gameObject );
-	}
+//	//======================================================
+//	public IEnumerator AutoDestroyCoroutine() {
+//		//wait for one frame to be on screen
+//		yield return null;
+//		float initialTime = MinigameTimeManager.instance.time;
+//		while( MinigameTimeManager.instance.time < initialTime + lifeSpan && IsOnScreen() ) {
+//			yield return null;
+//		}
+//		Destroy( gameObject );
+//	}
 
 	//======================================================
 	private bool IsOnScreen() {
@@ -69,6 +72,12 @@ public class ProjectileController : MonoBehaviour {
 
 	// ====================================================
 	void FixedUpdate() {
-		transform.position += VectorUtils.GetPosition3D( MinigameTimeManager.instance.fixedDeltaTime * direction * speed );
+		if( traveledDistance > maxTraveledDistance ) {
+			Destroy( gameObject );
+			return;
+		}
+		float deltaPos = MinigameTimeManager.instance.fixedDeltaTime * speed;
+		traveledDistance += deltaPos;
+		transform.position += VectorUtils.GetPosition3D( deltaPos * direction );
 	}
 }
